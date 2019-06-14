120b7d23e95c8c551211e05b45b5776390532c72
vjotchere <vjotchere@gmail.com>
2019-06-14 10:03:46 -0400

    updated tests

---
+++ spec/test_service.rb
@@ -0,0 +1,24 @@
+# require 'simplecov'
+# SimpleCov.start
+
+require_relative '../files/service'
+
+RSpec.describe Service do 
+    describe "#service_add" do
+        it "makes sure a service can be added" do
+            service = Service.new
+        end
+    end
+
+    describe "#service_add" do
+        it "verify input" do
+
+        end
+    end
+
+    describe "#service_remove" do
+        it "removes a service" do
+
+        end
+    end
+end
